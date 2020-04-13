71db43dd5bac947eb1adfdd2a4d4ed5a0e8015f1
Eric Xu <xujinzheng0610@gmail.com>
2020-04-13 23:48:08 +0800

    clean code

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ const Login = React.lazy(() => import('./views/Pages/Login'));
 const Register = React.lazy(() => import('./views/Pages/Register'));
 const Page404 = React.lazy(() => import('./views/Pages/Page404'));
 const Page500 = React.lazy(() => import('./views/Pages/Page500'));
-const Profile = React.lazy(() => import('./views/Profile'));
 
 function getCookie(name) {
   var nameEQ = name + "=";
